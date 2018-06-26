e0435330f2cad3bcad79dcbc193cbfa12613f2a8
Patricia <patchley02@gmail.com>
2018-06-26 14:14:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Patch"
+  end
+  
+  get '/hometown' do
+    "My hometown is Germantown"
+  end
 end
